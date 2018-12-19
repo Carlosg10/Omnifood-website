c5bc107e194ed719c2c87de3ab001b37c0f29160
Carlos Gonzales <42880381+Carlosg10@users.noreply.github.com>
2018-12-18 23:58:57 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-/* for the stocky navigation */
+/* for the sticky navigation */
   $('.js--section-features').waypoint(function(direction){
       if (direction == "down") {
           $('nav').addClass('sticky');
@@ -12,7 +12,7 @@ $(document).ready(function(){
 
 /* scroll on buttons */
 $('.js--scroll-to-plans').click(function () {
-    $('html', 'body').animate({scrollTop: $('.js--section-plans').offset(.top), 1000})
+    $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
 });
 
 
