860ffaf6b5dde72a403334d2336ecef2ecd48690
Carlos Gonzales <42880381+Carlosg10@users.noreply.github.com>
2018-12-18 23:45:06 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
+/* for the stocky navigation */
+  $('.js--section-features').waypoint(function(direction){
+      if (direction == "down") {
+          $('nav').addClass('sticky');
+      } else {
+         $('nav').removeClass('sticky');
+      }
+  }, {
+    offset: '60px;'
+  });
 
+/* scroll on buttons */
+$('.js--scroll-to-plans').click(function () {
+    $('html', 'body').animate({scrollTop: $('.js--section-plans').offset(.top), 1000})
+});
 
 
 
