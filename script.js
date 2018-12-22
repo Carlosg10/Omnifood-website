e72c67320cbdf794eb1d8a561404009e4fd1d8d5
Carlos Gonzales <42880381+Carlosg10@users.noreply.github.com>
2018-12-22 01:03:14 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -40,5 +40,10 @@ $(document).ready(function() {
       }
     });
   });
+/* Animations on scroll */
+
+$(.js--wp-1).waypoint(function(direction) {
+    $(.js--wp-1).addclass('animated fadeIn');
+})
 
 });
